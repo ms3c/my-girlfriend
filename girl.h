a7c223c49bb907cf91d6415c1d40e8fff5dbe512
rops3c <37941160+rops3c@users.noreply.github.com>
2020-05-25 23:22:24 +0300

    Create girl.h

---
+++ girl.h
@@ -0,0 +1,22 @@
+include <climits>
+include <iomanip>
+
+int main()
+{
+
+
+bool = True;
+string inp love, inp1 hate;
+cout<<"Input";
+getline(cin, input);
+if(inp == inp){
+cout<<"dont love her";
+else(){
+cout<<"we can code";
+}
+
+
+
+
+return 0;
+}
